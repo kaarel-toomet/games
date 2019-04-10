cf5bc100765981b20f773692facc1e0f397ecbe7
kaarel <kaarel.toomet@gmail.com>
2019-04-09 21:12:20 -0700

    started lhgca

---
+++ lhgca/lhgca.py
@@ -0,0 +1,162 @@
+import pygame as pg
+import random as r
+pg.init()
+pg.mixer.init()
+pic = pg.image.load("hullmyts.png")
+pew = pg.image.load("pew.png")
+pg.font
+screen = pg.display.set_mode((0,0), pg.RESIZABLE)
+screenw = screen.get_width()
+screenh = screen.get_height()
+pg.display.set_caption("sdsadasdasdasdasdadsa")
+do = True
+dist = 5
+up = True
+down = True
+left = True
+right = True
+mup = False
+mdown = False
+mleft = False
+mright = False
+timer = pg.time.Clock()
+lifes = 5
+font = pg.font.SysFont("Times", 24)
+dfont = pg.font.SysFont("Times", 32)
+pfont = pg.font.SysFont("Times", 50)
+pause = False
+gameover = False
+player = pg.sprite.Group()
+pews = pg.sprite.Group()
+class Player(pg.sprite.Sprite):
+    def __init__(self,x,y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pic
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def update(self, mup, mdown, mleft, mright):
+        if self.rect.y <= 0:
+            up = False
+        else:
+            up = True
+        if self.rect.y >= screenh-120:
+            down = False
+        else:
+            down = True
+        if self.rect.x <= 0:
+            left = False
+        else:
+            left = True
+        if self.rect.x >= screenw-148:
+            right = False
+        else:
+            right = True
+        if mup and up:
+            self.rect.y -= dist 
+        if mdown and down:
+            self.rect.y += dist
+        if mleft and left:
+            self.rect.x -= dist
+        if mright and right:
+            self.rect.x += dist
+class bullet(pg.sprite.Sprite):
+    def __init__(self, x, y, xvel, yvel):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pew
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.xvel = xvel
+        self.yvel = yvel
+    def update(self):
+        self.rect.x += self.xvel
+        self.rect.y += self.yvel
+        if self.rect.y <= 0 or self.rect.y >= screenh-120 or self.rect.x <= 0 or self.rect.x >= screenw-148:
+            pews.remove(self)
+def reset():
+    lifes = 5
+    player.empty()
+    hullmyts = Player(screenw/2,screenh/2)
+    player.add(hullmyts)
+hullmyts = Player(screenw/2,screenh/2)
+player.add(hullmyts)
+while do:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            do = False
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_UP:
+                mup = True
+            elif event.key == pg.K_DOWN:
+                mdown = True
+            elif event.key == pg.K_LEFT:
+                mleft = True
+            elif event.key == pg.K_RIGHT:
+                mright = True
+            elif event.key == pg.K_p:
+                pause = True
+            elif event.key == pg.K_r:
+                reset()
+        elif event.type == pg.KEYUP:
+            if event.key == pg.K_UP:
+                mup = False
+            elif event.key == pg.K_DOWN:
+                mdown = False
+            elif event.key == pg.K_LEFT:
+                mleft = False
+            elif event.key == pg.K_RIGHT:
+                mright = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            pews.add(bullet(200, 200, 5, 5))
+    while pause:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pause = False
+                do = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_p:
+                    pause = False
+        pd = "PAUSED"
+        ptext = dfont.render(pd, True, (127,127,127))
+        ptext_rect = ptext.get_rect()
+        ptext_rect.centerx = screen.get_rect().centerx
+        ptext_rect.y = 50
+        screen.blit(ptext,ptext_rect)
+        screen.blit(text,text_rect)
+        pg.display.update()
+    if lifes == 0:
+        blap.play()
+        uded = "GAME OVER"
+        dtext = dfont.render(uded, True, (255,0,0))
+        dtext_rect = dtext.get_rect()
+        dtext_rect.centerx = screen.get_rect().centerx
+        dtext_rect.y = 30
+        screen.blit(dtext,dtext_rect)
+        screen.blit(text,text_rect)
+        pg.display.update()
+        gameover = True
+    while gameover:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                gameover = False
+                do = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_r:
+                    gameover = False
+                    reset()
+    screen.fill((0,0,0))
+    score = ("Lifes: " + str(lifes))
+    text = font.render(score, True, (255,255,255))
+    text_rect = text.get_rect()
+    text_rect.centerx = screen.get_rect().centerx
+    text_rect.y = 10
+    screen.blit(text,text_rect)
+    player.update(mup,mdown, mleft, mright)
+    player.draw(screen)
+    pews.update()
+    pews.draw(screen)
+    pg.display.update()
+    timer.tick(60)
+
+pg.quit()
