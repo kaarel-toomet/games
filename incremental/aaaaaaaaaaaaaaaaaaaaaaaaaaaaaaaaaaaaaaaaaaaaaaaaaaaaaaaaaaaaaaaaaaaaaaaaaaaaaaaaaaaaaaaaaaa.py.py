6879949434a0eb07d6286cb24a67ad10c99bcbb2
kaarel <kaarel.toomet@gmail.com>
2020-03-16 22:48:45 -0700

    aaaa...aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.py: added save codes and started work on holding upgrades

--- incremental/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.py.py
+++ incremental/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.py.py
@@ -1,6 +1,36 @@
 import pygame as pg
 import random as r
 import numpy as np
+import re
+scode = list(input("Enter your save code, or 'x' for a new game:").split(",")) # 11 numbers separated by commas
+try:
+    scode = [int(x) for x in scode]
+    h = scode[0]
+    clvl = scode[1]
+    slvl = scode[2]
+    stkm = scode[3]
+    laser = scode[4]
+    llvl = scode[5]
+    prokoli = scode[6]
+    prkbase = scode[7]
+    prkbasecost = scode[8]
+    prkbaselvl = scode[9]
+    hlvl = scode[10]
+    hblvl = scode[11]
+except:
+    print("except")
+    h=0   
+    clvl = 0  #click lvl
+    slvl = 0
+    stkm = 0
+    laser = 0
+    llvl = 0
+    prokoli = 0
+    prkbase = 10 # the base of the log function used to generate prokoli amounts
+    prkbasecost = 2
+    prkbaselvl = 0
+    hlvl = 0  #hold level- 0=no holding, 1=hold to buy, 2=can hold on kitchen
+    hblvl = 0 # 
 pg.init()
 pg.mixer.init()
 btn = pg.image.load("button2.png")
@@ -14,32 +44,12 @@ screenw = screen.get_width()
 screenh = screen.get_height()
 pg.display.set_caption("trhtdsfthfghsfghsgfhsfdgtreytw")
 do = True
-dist = 5
-up = True
-down = True
-left = True
-right = True
-mup = False
-mdown = False
-mleft = False
-mright = False
 timer = pg.time.Clock()
-lifes = 5
-h = 0
-c = 0
-clvl = 0
-slvl = 0
+c = 0 # button thats clicked, 0=no buttons
 click = False
 hold = False
-stkm = 0
-laser = 0
-llvl = 0
-prokoli = 0
 bx = 0
 by = 0
-prkbase = 10
-prkbasecost = 2
-prkbaselvl = 0
 holdable = 0
 #txt = ("l", "厨房", str(3**clvl*200), str(10*1.1**stkm), str(1000*2**slvl), str(100*1.1**laser))
 font = pg.font.SysFont("Times", 24)
@@ -48,7 +58,7 @@ pfont = pg.font.SysFont("Times", 50)
 pause = False
 button = pg.sprite.Group()
 class Button(pg.sprite.Sprite):
-    def __init__(self,x,y, pic, n, clr=(64, 64, 64), size=21, still=False):
+    def __init__(self,x,y, pic, n, clr=(64, 64, 64), size=21, still=False, onetime = False):
         pg.sprite.Sprite.__init__(self)
         self.image = pic
         self.rect = self.image.get_rect()
@@ -60,6 +70,7 @@ class Button(pg.sprite.Sprite):
         self.fontsize = size
         self.n=n
         self.still = still
+        self.ot = onetime
     def update(self):
         global txt
         rtxt(self.rect.centerx, self.rect.centery, txt[self.n],
@@ -70,6 +81,9 @@ class Button(pg.sprite.Sprite):
     def clicked(self):
         global c
         c=self.n
+        if self.ot:
+            global button
+            button.remove(self)
 def reset():
     lifes = 5
     player.empty()
@@ -87,33 +101,27 @@ def rtxt(x,y,txt,color,size=20,still=False):
         text_rect.centerx = x-bx
         text_rect.centery = y-by
     screen.blit(text,text_rect)
-button.add(Button(screenw/2-100, screenh/2-100, thing, 1, (128, 128, 128), 30, True))#köök
-button.add(Button(100,100,btn,2,(255,255,255),20)) #click lvl
-button.add(Button(screenw-400,100,btn,3,(255,255,255),20))#stkm
-button.add(Button(100,200,btn,4,(255,255,255),20)) #stkm lvl
-button.add(Button(screenw-400,200,btn,5,(255,255,255),20)) #lasers
-button.add(Button(100,300,btn,6,(255,255,255),20)) #nonexist
-button.add(Button(screenw/2-100,screenh-100,btn,7,(255,0,0),20))#prestige
-button.add(Button(screenw-300,screenh-100,btn,8,(255,255,255),50)) # prokoli upgs
-button.add(Button(screenw+100,screenh-100,btn,9,(255,255,255),50)) # back
-button.add(Button(screenw+100,100,btn,10,(255,255,255),20)) #prokoli base upg
+b1 = Button(screenw/2-100, screenh/2-100, thing, 1, (128, 128, 128), 30, True)#köök
+b2 = Button(100,100,btn,2,(255,255,255),20) #click lvl
+b3 = Button(screenw-400,100,btn,3,(255,255,255),20)#stkm
+b4 = Button(100,200,btn,4,(255,255,255),20) #stkm lvl
+b5 = Button(screenw-400,200,btn,5,(255,255,255),20) #lasers
+b6 = Button(100,300,btn,6,(255,255,255),20) #nonexist
+b7 = Button(screenw/2-100,screenh-100,btn,7,(255,0,0),20,True)#prestige
+b8 = Button(screenw-300,screenh-100,btn,8,(255,255,255),50) # prokoli upgs
+b9 = Button(screenw+100,screenh-100,btn,9,(255,255,255),50) # back
+b10 = Button(screenw+100,100,btn,10,(255,255,255),20) #prokoli logbase upg
+b11 = Button(screenw+100,150,btn,11,(255,255,255),20,False) #prokoli hold upg buy
+b12 = Button(screenw+100,200,btn,12,(255,255,255),20,False) #prokoli hold upg köök
+b13 = Button(screenw+100,250,btn,13,(255,255,255),20,False) #prokoli click boost
+button.add(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13)
 while do:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             do = False
         elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_UP:
-                mup = True
-            elif event.key == pg.K_DOWN:
-                mdown = True
-            elif event.key == pg.K_LEFT:
-                mleft = True
-            elif event.key == pg.K_RIGHT:
-                mright = True
-            elif event.key == pg.K_p:
-                pause = True
-            elif event.key == pg.K_r:
-                reset()
+            if event.key == h:
+                holdable = 1-holdable
         elif event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1:
                 click = True
@@ -148,7 +156,9 @@ while do:
         pg.display.update()
     txt = ("l", "厨房", str(3**clvl*200), str(int(10*1.1**stkm)),
            str(1000*3**slvl),str(int(100*1.1**laser)), str(2**llvl*500),
-           "+" + str(int(np.log(h+0.1)/np.log(prkbase))-3) + " prokolit", ">","<", str(prkbasecost))
+           "+" + str(int(np.log(h+0.1)/np.log(prkbase))-3) + " prokolit",
+            ">","<", str(prkbasecost),"holdable buttons:3","holdable kitchen:10",
+            "helper click bonus:10")
     mc = pg.mouse.get_pos()
     bhps = (stkm*2**slvl + laser*(10+laser*llvl))*(1+prokoli/10)
     h+=bhps/60
@@ -174,7 +184,7 @@ while do:
     if c == 6 and h >= 2**llvl*500:
         h -= 2**llvl*500-10
         llvl += 1
-    if c == 7 and h >= 10000:
+    if c == 7 and int(np.log(h)/np.log(prkbase))-3 >= 1:
         prokoli += int(np.log(h)/np.log(prkbase))-3
         h = 0
         llvl = 0
@@ -191,6 +201,18 @@ while do:
         prkbasecost = int(prkbasecost*1.5)
         prkbase = 1 + (prkbase-1)*0.9
         prkbaselvl += 1
+    if c == 11 and prokoli >= 5:
+        prokoli -= 5
+        hlvl = 1
+        button.remove(b11)
+    if c == 12 and prokoli >= 10:
+        prokoli -= 10
+        hlvl = 2
+        button.remove(b12)
+    if c == 13 and prokoli >= 10:
+        prokoli -= 10
+        hlvl = 2
+        button.remove(b12)
     rtxt(200,60,"richer kitchens: " + str(clvl),(255,255,255))
     rtxt(200,80,"doubles click base",(255,255,255))
     rtxt(200,160,"better axes: " + str(slvl),(255,255,255))
@@ -203,11 +225,12 @@ while do:
     rtxt(screenw-300,160,"laser: " + str(laser),(255,255,255))
     rtxt(screenw-300,180,"good for breaking kitchens, + " + str(10+laser*llvl) + " hps",(255,255,255))
     rtxt(screenw-300,20,"SHOP",(255,255,255), 50)
-    rtxt(screenw/2,10,"h: " + str(int(h)),(255,255,255))
-    rtxt(screenw/2,30,"hps: " + str(int(bhps)),(255,255,255))
-    rtxt(screenw/2,50,"h/click: " + str(2**clvl),(255,255,255))
-    rtxt(screenw/2,70,"prokoli: " + str(prokoli),(255,255,255))
-    rtxt(screenw/2,screenh-150,"PRESTIGE",(255,255,255))
+    rtxt(screenw/2,10,"h: " + str(int(h)),(255,255,255),20,True)
+    rtxt(screenw/2,30,"hps: " + str(int(bhps)),(255,255,255),20,True)
+    rtxt(screenw/2,50,"h/click: " + str(2**clvl),(255,255,255),20,True)
+    rtxt(screenw/2,70,"prokoli: " + str(prokoli),(255,255,255),20,True)
+    rtxt(screenw/2,70,"holdable: " + holdable*"yes"+(1-holdable)*"no" + " (H)",(255,255,255),20,True)
+    rtxt(screenw/2,screenh-150,"PRESTIGE",(255,0,0),20,True)
     rtxt(screenw-200,screenh-150,"prokoli upgrades",(255,255,255))
     rtxt(screenw+200,screenh-150,"back",(255,255,255))
     rtxt(screenw+200,50,"Reduce prokoli logarithm base - 1 by 10%" + ":" + str(prkbaselvl),(255,255,255))
@@ -222,3 +245,5 @@ while do:
     timer.tick(60)
 
 pg.quit()
+scode = (int(h),clvl,slvl,stkm,laser,llvl,prokoli,prkbase,prkbasecost,prkbaselvl,hlvl)
+print("Here's your save code (without parentheses):",scode)
