8017dc41a323628b60fc9b5294c7548b852e34e0
kaarel <kaarel.toomet@gmail.com>
2020-03-23 19:23:10 +0200

    health crafting, cheaper activators, and enter to go underground

--- uwu749/uwu749.py
+++ uwu749/uwu749.py
@@ -399,7 +399,7 @@ while do:
                 gmod = 1-gmod
             elif event.key == pg.K_t:
                 title = True
-            elif event.key == pg.K_SLASH:
+            elif event.key == 13:
                 if globals.activeWindow[coordinates.worldToWindow(globals.hullmyts.getxy()[0],globals.hullmyts.getxy()[1])[1],coordinates.worldToWindow(globals.hullmyts.getxy()[0],globals.hullmyts.getxy()[1])[0]] == blocks.AUK:
                     if globals.activelayer is globals.ground:
                         globals.activelayer = globals.underground
@@ -429,9 +429,12 @@ while do:
                 elif gameState.inventory[select] == blocks.GORE:
                     lose(blocks.GORE)
                     gameState.kuld += 2
-                elif gameState.inventory[select] == blocks.BORE and gameState.amounts[select] >= 5:
+                elif gameState.inventory[select] == blocks.BORE and gameState.amounts[select] >= 2:
                     get(blocks.AACT)
-                    lose(blocks.BORE, 5)
+                    lose(blocks.BORE, 2)
+                elif gameState.inventory[select] == blocks.CORE and gameState.amounts[select] >= 5:
+                    gameState.lifes += 1
+                    lose(blocks.CORE, 5)
                 elif gameState.inventory[select] == blocks.KUKS:
                     get(blocks.AED)
                     lose(blocks.KUKS)
